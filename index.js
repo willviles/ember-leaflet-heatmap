b951fa557e1e811f7970657c1f41a39ab5941aa4
willviles <will@willviles.com>
2016-05-27 12:51:37 +0100

    Fixes path to leaflet.heatmap.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ module.exports = {
   name: 'ember-leaflet-heatmap',
   included: function(app){
     app.import(app.bowerDirectory + '/heatmap.js-amd/build/heatmap.js');
-  	app.import('/vendor/leaflet.heatmap.js');
+  	app.import('vendor/leaflet.heatmap.js');
   }
 };
