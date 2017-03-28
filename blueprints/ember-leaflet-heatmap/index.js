8e1384ec6daac6d4f0b1476404401911df3ea20b
Will Viles <will@willviles.com>
2017-03-28 12:26:20 +0100

    Adds normalizeEntityName to blueprint

--- blueprints/ember-leaflet-heatmap/index.js
+++ blueprints/ember-leaflet-heatmap/index.js
@@ -1,6 +1,16 @@
+/*jshint node:true*/
+'use strict';
+
 module.exports = {
+
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
+
   afterInstall: function() {
     return this.addBowerPackageToProject("heatmap.js-amd", "*");
   }
-  
+
 };
